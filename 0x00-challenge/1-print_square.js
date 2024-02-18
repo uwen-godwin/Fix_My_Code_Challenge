4ba901c31ba5f2e47f584e70403a9f832d7082ed
uwen-godwin <uwengodwin001@gmail.com>
2024-02-17 19:26:57 -0800

    Fix my code

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,9 +1,24 @@
-#!/usr/bin/env node
-const size = parseInt(process.argv[2]);
-if (isNaN(size)) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < size; i++) {
-    console.log('#'.repeat(size));
-  }
+#!/usr/bin/node
+/*
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
+*/
+
+
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
+}
+
+const size = parseInt(process.argv[2]); // Use base 10 for parsing the integer
+
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
 }
