54cf5f5b28d0ed85190a3b775415cf617868380c
uwen-godwin <uwengodwin001@gmail.com>
2024-02-17 20:12:51 -0800

    Fix my code

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,42 +2,42 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Delete a node at a specific index from a list
- *
- * @head: A pointer to the first element of a list
- * @index: The index of the node to delete
- *
- * Return: 1 on success, -1 on failure
- */
+* delete_dnodeint_at_index - Delete a node at a specific index from a list
+*
+* @head: A pointer to the first element of a list
+* @index: The index of the node to delete
+*
+* Return: 1 on success, -1 on failure
+*/
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head;
-    dlistint_t *temp = NULL;
-    unsigned int i;
+dlistint_t *current = *head;
+dlistint_t *temp = NULL;
+unsigned int i;
 
-    if (*head == NULL)
-        return (-1);
+if (*head == NULL)
+return (-1);
 
-    if (index == 0)
-    {
-        *head = current->next;
-        if (current->next != NULL)
-            current->next->prev = NULL;
-        free(current);
-        return (1);
-    }
+if (index == 0)
+{
+*head = current->next;
+if (current->next != NULL)
+current->next->prev = NULL;
+free(current);
+return (1);
+}
 
-    for (i = 0; current != NULL && i < index - 1; i++)
-        current = current->next;
+for (i = 0; current != NULL && i < index - 1; i++)
+current = current->next;
 
-    if (current == NULL || current->next == NULL)
-        return (-1);
+if (current == NULL || current->next == NULL)
+return (-1);
 
-    temp = current->next->next;
-    free(current->next);
-    current->next = temp;
-    if (temp != NULL)
-        temp->prev = current;
+temp = current->next->next;
+free(current->next);
+current->next = temp;
+if (temp != NULL)
+temp->prev = current;
 
-    return (1);
+return (1);
 }
--- 0x00-challenge/4-delete_dnodeint/main.c
+++ 0x00-challenge/4-delete_dnodeint/main.c
@@ -4,6 +4,48 @@
 #include "lists.h"
 
 /**
+ * add_nodes - Add nodes to the end of the list.
+ * @head: Pointer to the head of the list.
+ */
+void add_nodes(dlistint_t **head)
+{
+	add_dnodeint_end(head, 0);
+	add_dnodeint_end(head, 1);
+	add_dnodeint_end(head, 2);
+	add_dnodeint_end(head, 3);
+	add_dnodeint_end(head, 4);
+	add_dnodeint_end(head, 98);
+	add_dnodeint_end(head, 402);
+	add_dnodeint_end(head, 1024);
+}
+
+/**
+ * delete_nodes - Delete nodes from the list.
+ * @head: Pointer to the head of the list.
+ */
+void delete_nodes(dlistint_t **head)
+{
+	delete_dnodeint_at_index(head, 5);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+	delete_dnodeint_at_index(head, 0);
+}
+
+/**
  * main - check the code for Holberton School students.
  *
  * Return: Always EXIT_SUCCESS.
@@ -13,56 +55,10 @@ int main(void)
 	dlistint_t *head;
 
 	head = NULL;
-	add_dnodeint_end(&head, 0);
-	add_dnodeint_end(&head, 1);
-	add_dnodeint_end(&head, 2);
-	add_dnodeint_end(&head, 3);
-	add_dnodeint_end(&head, 4);
-	add_dnodeint_end(&head, 98);
-	add_dnodeint_end(&head, 402);
-	add_dnodeint_end(&head, 1024);
+	add_nodes(&head);
 	print_dlistint(head);
 	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 5);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
+	delete_nodes(&head);
 	print_dlistint(head);
 	free_dlistint(head);
 	return (0);
